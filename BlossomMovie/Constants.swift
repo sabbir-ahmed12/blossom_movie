ab1d4e33dd77d72e544f9a364a41a7960b56be58
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2026-03-03 15:37:27 -0600

    Added placeholder hero image and buttons in the home screen.

--- BlossomMovie/Constants.swift
+++ BlossomMovie/Constants.swift
@@ -6,15 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Constants {
     static let homeString = "Home"
     static let upcomingString = "Upcoming"
     static let searchString = "Search"
     static let downloadString = "Download"
+    static let playString = "Play"
     
     static let homeIconString = "house"
     static let upcomingIconString = "play.circle"
     static let searchIconString = "magnifyingglass"
     static let downloadIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
+}
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background{
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .stroke(.buttonBorder, lineWidth: 2)
+            }
+    }
 }
---
+++ BlossomMovie/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  BlossomMovie
+//
+//  Created by Sabbir Ahmed on 3/3/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var heroTestTitle = Constants.testTitleURL
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: heroTestTitle)){image in image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text(Constants.playString)
+                        .ghostButton()
+                }
+                Button {
+                    
+                } label: {
+                    Text(Constants.downloadString)
+                        .ghostButton()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
