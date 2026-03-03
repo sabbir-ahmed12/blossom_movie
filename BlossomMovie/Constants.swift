05444668f7f43571733e5604f5ac293d7be3cde2
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2026-03-03 15:03:13 -0600

    Refactored ContentView by separating hardcoded strings.

---
+++ BlossomMovie/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  BlossomMovie
+//
+//  Created by Sabbir Ahmed on 3/3/26.
+//
+
+import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+}
