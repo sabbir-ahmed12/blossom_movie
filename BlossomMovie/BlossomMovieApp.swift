f49765f62ff012f298954cf27a0691497dec2dad
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2026-03-03 14:46:01 -0600

    Completed initial setup of the project.

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Sabbir Ahmed on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
